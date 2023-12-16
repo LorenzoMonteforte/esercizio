7faebaf606091ab6245cfdad4435506c6fd7e105
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-16 16:38:43 +0100

    Header (Menu)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    name: "Navbar",
+    props: {
+        navbarItems: Array
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li v-for="navbarItem in navbarItems">
+                <strong>
+                    {{ navbarItem.title }}
+                    <font-awesome-icon class="marLef0_5rem" icon="fa-solid fa-caret-down" v-if="navbarItem.arrow == true" />
+                </strong>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+nav>ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+}
+
+nav>ul>li:first-of-type {
+    color: rgba(232, 210, 89, 255);
+}
+
+nav {
+    width: 100%;
+}
+
+nav>ul>li {
+    color: white;
+}
+</style>
