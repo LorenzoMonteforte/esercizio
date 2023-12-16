14bc22a4cba8f6186d6d1711c0fa853cf88bd40f
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-16 18:59:52 +0100

    Header (Carousel)

---
+++ src/components/Carousel.vue
@@ -0,0 +1,124 @@
+<script>
+export default {
+    name: "Carousel",
+    props: {
+        dataCarousel: Array
+    },
+    data() {
+        return {
+            indexCarousel: 0
+        }
+    },
+    methods: {
+        avanti: function () {
+            if (this.indexCarousel == (this.dataCarousel.length - 1)) {
+                this.indexCarousel = 0;
+            } else {
+                this.indexCarousel++;
+            }
+        },
+        indietro: function () {
+            if (this.indexCarousel == 0) {
+                this.indexCarousel = (this.dataCarousel.length - 1);
+            } else {
+                this.indexCarousel--;
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="carousel">
+        <div>
+            <button class="lefRig" @click="indietro(), $emit('invokeChangeBackground', this.indexCarousel)">
+                <font-awesome-icon icon="fa-solid fa-chevron-left" />
+            </button>
+        </div>
+        <div>
+            <h1>{{ dataCarousel[indexCarousel].title }}</h1>
+            <p>{{ dataCarousel[indexCarousel].paragraph }}</p>
+            <div>
+                <button class="btn btnYel">
+                    <strong>
+                        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                        Search courses
+                    </strong>
+                </button>
+                <button class="btn btnWhi">
+                    <strong>
+                        <font-awesome-icon icon="fa-solid fa-user-plus" />
+                        Apply for university
+                    </strong>
+                </button>
+            </div>
+        </div>
+        <div>
+            <button class="lefRig" @click="avanti(), $emit('invokeChangeBackground', this.indexCarousel)">
+                <font-awesome-icon icon="fa-solid fa-chevron-right" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#carousel {
+    height: 100%;
+    width: 100%;
+    display: flex;
+}
+
+#carousel>div:first-child,
+#carousel>div:last-child {
+    width: 25%;
+    position: relative;
+}
+
+#carousel>div:nth-child(2) {
+    width: 50%;
+    padding: 3rem 0;
+}
+
+.lefRig {
+    position: absolute;
+    top: 50%;
+    translate: 0 -50%;
+    font-size: 2.5rem;
+    border: none;
+    color: white;
+    background-color: transparent;
+}
+
+#carousel>div:first-child>button {
+    left: -1rem;
+}
+
+#carousel>div:nth-child(2)>h1 {
+    text-align: center;
+    font-size: 3rem;
+    color: white;
+}
+
+#carousel>div:nth-child(2)>p {
+    margin-top: 3rem;
+    text-align: center;
+    font-size: 1.25rem;
+    color: white;
+}
+
+#carousel>div:nth-child(2)>div {
+    margin-top: 3rem;
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+}
+
+.btnWhi {
+    color: rgba(218, 108, 110, 255);
+    background-color: white;
+}
+
+#carousel>div:last-child>button {
+    right: -1rem;
+}
+</style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,11 @@
 <script>
 import Navbar from './Navbar.vue';
+import Carousel from './Carousel.vue';
 export default {
     name: "PageHeader",
     components: {
-        Navbar
+        Navbar,
+        Carousel
     },
     data() {
         return {
@@ -36,25 +38,56 @@ export default {
                     title: "Purchase",
                     arrow: false
                 }
+            ],
+            dataCarousel: [
+                {
+                    title: "Showcase your courses",
+                    paragraph: "EduPrime is the most versatile WordPress theme for educational purpose, showcasing universities, courses, secondary school etc.",
+                    srcBackground: "img/theme_slider1_bg-1.jpg"
+                },
+                {
+                    title: "Key to your success",
+                    paragraph: "EduPrime is the most versatile WordPress theme for educational purpose, showcasing universities, courses, secondary school etc.",
+                    srcBackground: "img/theme_slider2_bg-1.jpg"
+                },
+                {
+                    title: "Lead. Inspire. Win!",
+                    paragraph: "EduPrime is the most versatile WordPress theme for educational purpose, showcasing universities, courses, secondary school etc.",
+                    srcBackground: "img/theme_slider3_bg-1.jpg"
+                }
             ]
+        }
+    },
+    methods: {
+        getImagePath: function (img) {
+            return new URL(`../assets/${img}`, import.meta.url).href;
+        },
+        changeBackground: function (indexCarousel) {
+            const header = document.getElementById("header");
+            header.style.backgroundImage = "url(" + this.getImagePath(this.dataCarousel[indexCarousel].srcBackground) + ")"
         }
     }
 }
 </script>
 
 <template>
-    <header>
-        <div>
+    <header id="header">
+        <div id="opaRed">
             <div>
-                <img src="../assets/img/theme_eduprime_logo.png" alt="Logo Eduprime">
+                <div>
+                    <img src="../assets/img/theme_eduprime_logo.png" alt="Logo Eduprime">
+                </div>
+                <div>
+                    <Navbar :navbarItems="navbarItems" />
+                </div>
+                <div>
+                    <button class="btn btnYel">
+                        <strong>VIEW COURSES</strong>
+                    </button>
+                </div>
             </div>
             <div>
-                <Navbar :navbarItems="navbarItems" />
-            </div>
-            <div>
-                <button>
-                    <strong>VIEW COURSES</strong>
-                </button>
+                <Carousel :dataCarousel="dataCarousel" @invokeChangeBackground="changeBackground" />
             </div>
         </div>
     </header>
@@ -64,47 +97,47 @@ export default {
 header {
     height: 1000px;
     background-color: red;
-    padding: 3rem 3rem 0;
 }
 
-header>div:first-child {
+#opaRed>div:first-child {
     display: flex;
 }
 
-header>div:first-child>div:first-child,
-header>div:first-child>div:last-child {
+#opaRed>div:first-child>div:first-child,
+#opaRed>div:first-child>div:last-child {
     width: 25%;
 }
 
-header>div:first-child>div:nth-child(2) {
+#opaRed>div:first-child>div:nth-child(2) {
     width: 50%;
     display: flex;
     align-items: center;
 }
 
-header>div:first-child>div:first-child>img {
+#opaRed>div:first-child>div:first-child>img {
     width: 100%;
 }
 
-header>div:first-child>div:first-child {
+#opaRed>div:first-child>div:first-child {
     padding-right: 1.5rem;
     display: flex;
     align-items: center;
 }
 
-header>div:first-child>div:last-child {
+#opaRed>div:first-child>div:last-child {
     display: flex;
     align-items: center;
     padding-left: 1.5rem;
     justify-content: flex-end;
 }
 
-header>div:first-child>div:last-child>button {
-    font-size: 1rem;
-    padding: 1.5rem 3rem;
-    border: none;
-    border-radius: 2rem;
-    color: white;
-    background-color: rgba(232, 210, 89, 255);
+#opaRed>div:last-child {
+    margin-top: 6rem;
+}
+
+#opaRed {
+    height: 100%;
+    background-color: rgba(218, 108, 110, 0.9);
+    padding: 3rem 3rem 0;
 }
 </style>
